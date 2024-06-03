5b454bea73d6928c7471306290adbf25f4d8a055
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-06-03 10:29:10 +0700

    penggunakan try and catch

---
+++ bagian3/bagian3.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <exception>
+
+#include <array>
+
+using namespace std;
+
+int main()
+{
+	cout << "awal program " << endl;
+	try {
+		array <int, 3> data = { 1,2,3 };
+		cout << data.at(5) << endl;
+	}
+	catch (exception& e) {
+		cout << e.what() << endl;
+	}
+
+	cout << "baris program yang terakhir " << endl;
+
+}
+
